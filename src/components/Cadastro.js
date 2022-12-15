338a832c8b5191b8d2bbc68d19da83a5df8d65e0
dutra-86 <gabrielsprecherdutra@gmail.com>
2022-12-15 10:38:03 -0300

    Tela de cadastro finalizada

--- src/components/Cadastro.js
+++ src/components/Cadastro.js
@@ -1,5 +1,123 @@
+import {useState, useEffect} from 'react'
+import styled from 'styled-components'
+import { BrowserRouter, Routes, Route, Link, useNavigate } from "react-router-dom"
+import axios from 'axios'
+
+import logo from '../assets/logo.png'
+
 export default function Cadastro(props){
-    return(<>
-        
-    </>)
+
+	const nav = useNavigate()
+
+	const[userEmail, setUserEmail] = useState("")
+	const[password, setPassword] = useState("")
+	const[nome, setNome] = useState("")
+	const[foto, setFoto] = useState("")
+
+	const[disabled, setDisabled] = useState(false);
+
+	const [form, setForm] = useState({
+		email: '',
+		name: '',
+		image: '',
+		password: ''
+	})
+
+	function handleForm (e) {
+		setForm({
+			...form,
+			[e.target.name]: e.target.value,
+		}) 
+	}
+
+	function submitSignIn(event){
+		event.preventDefault();
+		setDisabled(true);
+		const prom = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up', form);
+
+		prom.then(()=>nav("/"));
+		prom.catch(()=>setDisabled(false));
+	}
+
+	return(<Display>
+		<img src={logo}/>
+		
+		<Form>
+			<form onSubmit={submitSignIn}>
+				<input required type="email" name="email" value={form.email} onChange={handleForm} placeholder="email" disabled={disabled}/>
+
+				<input required type="text" name="password" value={form.password} onChange={handleForm} placeholder="senha" disabled={disabled}/>
+
+				<input required type="text" name="name" value={form.name} onChange={handleForm} placeholder="nome" disabled={disabled}/>
+
+				<input required type="url" name="image" value={form.image} onChange={handleForm} placeholder="foto" disabled={disabled}/>
+
+				<button type="submit" disabled={disabled}>
+						Cadastrar
+				</button>
+			</form>
+		</Form>
+		<Link to="/"><span>Já tem uma conta? Faça login!</span></Link>
+	</Display>)
 }
+
+const Display = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	img{
+		margin-top: 68px;
+		margin-bottom: 32px;
+		width: 180px;
+		height: auto;
+	}
+	span{
+		color: #52B6FF;
+		text-decoration: underline;
+		font-family: Lexend Deca;
+		font-size: 14px;
+		font-weight: 400;
+		line-height: 17px;
+		letter-spacing: 0em;
+		text-align: center;
+	}
+`
+
+const Form = styled.div`
+	form{
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		input{
+			border: 1px solid #D4D4D4;
+			height: 45px;
+			width: 303px;
+			border-radius: 5px;
+			margin-bottom: 6px;
+			font-family: Lexend Deca;
+			font-size: 20px;
+			font-weight: 400;
+			line-height: 25px;
+			letter-spacing: 0em;
+			text-align: left;
+			padding-left: 11px;
+			box-sizing: border-box;
+		}
+		button{
+			background: #52B6FF;
+			font-family: Lexend Deca;
+			font-size: 21px;
+			font-weight: 400;
+			line-height: 26px;
+			letter-spacing: 0em;
+			text-align: center;
+			height: 45px;
+			width: 303px;
+			border-radius: 4.64px;
+			border: none;
+			color: white;
+			margin-bottom: 25px;
+		}
+	}
+`
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,52 +1,93 @@
 import {useState, useEffect} from 'react'
 import styled from 'styled-components'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
 import logo from '../assets/logo.png'
 
 export default function Main(props){
 
-    const[userEmail, setUserEmail] = useState("")
-    const[password, setPassword] = useState("")
+	const[userEmail, setUserEmail] = useState("")
+	const[password, setPassword] = useState("")
 
-    function submitLogin(){
+	function submitLogin(){
 
-    }
+	}
 
-    return(<Display>
-        <img src={logo}/>
-        
-        <Form>
-            <form onSubmit={submitLogin}>
-                <input required type="text" value={userEmail} onChange={e => setUserEmail(e.target.value)} placeholder="email"/>
+	return(<Display>
+		<img src={logo}/>
+		
+		<Form>
+			<form onSubmit={submitLogin}>
+				<input required type="text" value={userEmail} onChange={e => setUserEmail(e.target.value)} placeholder="email"/>
 
-                <input required type="text" value={password} onChange={e => setpassword(e.target.value)} placeholder="senha"/>
+				<input required type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="senha"/>
 
-                <button type="submit">
-                        Entrar
-                    </button>
-            </form>
-        </Form>
-    </Display>)
+				<button type="submit">
+						Entrar
+				</button>
+			</form>
+		</Form>
+		<Link to="/cadastro"><a>Não tem uma conta? Cadastre-se!</a></Link>
+	</Display>)
 }
 
 const Display = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    img{
-        margin-top: 68px;
-        margin-bottom: 32px;
-        width: 180px;
-        height: auto;
-    }
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	img{
+		margin-top: 68px;
+		margin-bottom: 32px;
+		width: 180px;
+		height: auto;
+	}
+	a{
+		color: #52B6FF;
+		text-decoration: underline;
+		font-family: Lexend Deca;
+		font-size: 14px;
+		font-weight: 400;
+		line-height: 17px;
+		letter-spacing: 0em;
+		text-align: center;
+	}
 `
 
 const Form = styled.div`
-    form{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
+	form{
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		input{
+			border: 1px solid #D4D4D4;
+			height: 45px;
+			width: 303px;
+			border-radius: 5px;
+			margin-bottom: 6px;
+			font-family: Lexend Deca;
+			font-size: 20px;
+			font-weight: 400;
+			line-height: 25px;
+			letter-spacing: 0em;
+			text-align: left;
+			padding-left: 11px;
+			box-sizing: border-box;
+		}
+		button{
+			background: #52B6FF;
+			font-family: Lexend Deca;
+			font-size: 21px;
+			font-weight: 400;
+			line-height: 26px;
+			letter-spacing: 0em;
+			text-align: center;
+			height: 45px;
+			width: 303px;
+			border-radius: 4.64px;
+			border: none;
+			color: white;
+			margin-bottom: 25px;
+		}
+	}
 `
